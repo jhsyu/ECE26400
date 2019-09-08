bbf3d0fe3af326687a8df1759332f95ad7e07b11
kiakohsyu <xu1392@purdue.edu>
2019-09-08 15:51:38 -0400

    finished.

--- solutions/Part1/HW05Sum/fileint.c
+++ solutions/Part1/HW05Sum/fileint.c
@@ -8,14 +8,33 @@
 #ifdef TEST_ADDFILE
 bool addFile(char * filename, int * sum)
 {
-  // You cannot assume * sum is zero. Thus, * sum needs to be set 0
+  // You cannot assume * sum is zero.
+  // Thus, * sum needs to be set 0
   // open a file whose name is filename for reading
   // if fopen fails, return false. Do NOT fclose
-  // if fopen succeeds, read integers using fscan (do not use fgetc)
+  // if fopen succeeds, read integers using fscan
+  // (do not use fgetc)
   //
   // * sum stores the result of adding all numbers from the file
   // When no more numbers can be read, fclose, return true
   //
+  FILE* fptr = fopen(filename, "r");
+  if (fptr == NULL)
+    return false;
+  int add;
+  *sum = 0; 
+  while ((fscanf(fptr, "%d", &add)) != EOF)
+    {
+      if (add != EOF)
+	*sum = *sum + add;
+    }
+  if (fptr != NULL)
+    {
+      fclose(fptr);
+      fptr = NULL;
+    }
+  
+  
   return true;
 }
 #endif
@@ -29,6 +48,16 @@ bool writeSum(char * filename, int sum)
   // fprintf should use one newline '\n'
   // fclose, return true
   //
+  FILE* fptr = fopen(filename, "w");
+  if (fptr != NULL)
+    {
+      fprintf(fptr, "%d\n", sum);
+      fclose(fptr);
+    }
+  else
+    {
+      return false;
+    }
   return true;
 }
 #endif

4304b8439ce560dd8e68e3b6134685bf020b91b9
kiakohsyu <xu1392@purdue.edu>
2019-09-14 22:22:15 -0400

    finiished

--- solutions/Part1/HW06Word/filestr.c
+++ solutions/Part1/HW06Word/filestr.c
@@ -13,25 +13,51 @@ int countWord(char * filename, char * word, char * line, int size)
   // word: the word to search
   // line: temporary space to store what is read from the file
   // size: size of line
+  
   // open a file whose name is filename for reading
   // if fopen fails, return -1. 
   // if fopen succeeds, set sum to zero
   // use fgets to read the file
   // if word appears in a line, add one to sum
   //
-  // It is possible that the same word appears multiple times in a line
-  // If this word is split in two or more lines, do not count the word.
+  // It is possible that the same word appears multiple times
+  // in a line
+  // If this word is split in two or more lines,
+  // do not count the word.
   //
-  // It is also possible that a long line is split by fgets. If this happens,
+  // It is also possible that a long line is split by fgets.
+  // If this happens,
   // do not count the word.
   //
   // return sum
   //
-  // If a line is "aaaaa" and the word is "aa", how is the count defined?
-  // In this assignment, the first two letters are consumed when the
-  // the first match occurs. Thus, the next match starts at the third
+  // If a line is "aaaaa" and the word is "aa",
+  // how is the count defined?
+  // In this assignment, the first two letters are consumed
+  // when the first match occurs.
+  // Thus, the next match starts at the third
   // character. For this case, the correct answer is 2, not 4.
+
+  //the word, the string and it size,filename are known.
+
+  FILE* fptr = fopen(filename, "r");
+  if(fptr == NULL)
+    return -1;
+  int sum = 0;
+  int ind = 0;
+  char* pos;
   
-  return 0;
+   while ((line = fgets(line, size, fptr)) != NULL)
+    {
+      ind = 0;
+      while ((pos = strstr ((line + ind), word)) != NULL)
+	{
+	  sum ++;
+	  ind = pos - line + strlen(word);
+	}
+    }
+  
+  
+  return sum;
 }
 #endif

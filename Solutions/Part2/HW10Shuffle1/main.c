63392dec0cc87310f23499a3050486b9226baee9
Jacob Hsyu <xu1392@purdue.edu>
2019-10-07 13:04:13 -0400

    add comment to main,c

--- Solutions/Part2/HW10Shuffle1/main.c
+++ Solutions/Part2/HW10Shuffle1/main.c
@@ -18,7 +18,14 @@ int main(int argc, char ** argv)
       return EXIT_FAILURE;
     }
 
+  //convert the string argv[1] to a number.
+  //argv[1] is the number of the cards.
+
+  //strtol convert the everynumber in a string
+  //to a part of the number converted,
+  //untill there is a non-number character.
   int size = (int) strtol(argv[1], NULL, 10);
+
   if ((size <= 0) || (size > MAX_SIZE))
     {
       return EXIT_FAILURE;
@@ -34,10 +41,13 @@ int main(int argc, char ** argv)
   // origDeck.size = MAX_SIZE;
 
   origDeck.size = size;
+
+  //apply the real situation to the array cards.
+  //origDeck.cards is the address of the array.
+  //void * memcpy(void *to, const void *from, size_t numBytes);
   memcpy(origDeck.cards, deck, MAX_SIZE * sizeof(origDeck.cards[0]));
 
   shuffle(origDeck);
-  
+
   return EXIT_SUCCESS;
 }
-

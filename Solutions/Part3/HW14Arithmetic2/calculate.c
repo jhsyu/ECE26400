6ab6d92e72ded6464904df09515a41470c315706
xu1392 <xu1392@purdue.edu>
2019-10-30 15:53:50 -0400

    copy the last assignment to this project

---
+++ Solutions/Part3/HW14Arithmetic2/calculate.c
@@ -0,0 +1,162 @@
+// ***
+// *** You MUST modify this file
+// ***
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "calculate.h"
+
+// DO NOT MODIFY FROM HERE --->>>
+const int Operations[] = {'+', '-', '*'};
+
+// return -1 if the word is not an operator
+// return 0 if the word contains '+'
+// return 1 if the word contains '-'
+// return 2 if the word contains '*'
+int isOperator(char * word)
+{
+  int ind;
+  int numop = sizeof(Operations) / sizeof(int);
+  for (ind = 0; ind < numop; ind ++)
+    {
+    char *loc = strchr(word, Operations[ind]);
+    if (loc != NULL && !isdigit(loc[1]))
+	{
+	  return ind;
+	}
+    }
+  return -1;
+}
+// <<<--- UNTIL HERE
+
+// ***
+// *** You MUST modify the calculate function
+// ***
+#ifdef TEST_CALCULATE
+// if arithlist is NULL, return true
+// if arithlist -> head is NULL, return true
+// if the input list is invalid, return false
+bool calculate(List * arithlist)
+{
+  if (arithlist == NULL)
+    {
+      return true;
+    }
+  if ((arithlist -> head) == NULL)
+    {
+      return true;
+    }
+    int numE = 1;
+    int numN = 0;
+    int result = 0;
+    ListNode * p = arithlist -> head;
+    ListNode * q = p -> next;
+    ListNode * o = NULL;
+    if ((q == NULL) && (isOperator(p -> word) == -1)) {
+      return true;
+    }
+    while (q != NULL) {
+      numE ++;
+      if (isOperator(p -> word) == -1) {
+        numN ++;
+      }
+      p = q;
+      q = p -> next;
+    }
+    // initialize p and q
+    while (numN != 1) {
+      p = arithlist -> head;
+      q = p -> next;
+      // operator cannot appear in the first place.
+      if (isOperator(p -> word) != -1) {
+        return false;
+      }
+      // find a operator.
+      while ((q != NULL) && (isOperator(q -> word) == -1)) {
+        // must check if q is null.
+        // these two lines cannot change the order.
+        p = q;
+        q = p -> next;
+      }
+      if (q == NULL) {
+        // no operator found.
+        return false;
+      }
+      //there is an operator found: q is an operator.
+      // hence, p is the second number, p -> prev is the first number.
+      // need conversion from '1' to 1.
+      // find the two number before it.
+      o = p -> prev;
+      // o points to the first number.
+      if (p != arithlist -> head) {
+        // cannot find two numbers before the operator.
+        if (o == NULL) {
+          return false;
+        }
+        if (isOperator(o -> word) != -1) {
+          return false;
+        }
+        if (isOperator(p -> word) != -1) {
+          return false;
+        }
+      }
+      if ((o != NULL) && (p != NULL)) {
+        int a = (int) strtol(o -> word, NULL, 10);
+        int b = (int) strtol((p -> word), NULL, 10);
+        int rtv = isOperator(q -> word);
+        switch (rtv) {
+          case 0:
+          result = a + b;
+          break;
+          case 1:
+          result = a - b;
+          break;
+          case 2:
+          result = a * b;
+          break;
+          default:
+          result = - 114514;
+          return false;
+        }
+        sprintf(q -> word, "%d\n", result);
+        // after calculation, push the result to the linked list
+        // and delete the numbers and opertors.
+        deleteNode(arithlist, o);
+        deleteNode(arithlist, p);
+        numE -= 2;
+        numN --;
+      }
+
+    }
+    if (numE != numN) {
+      return false;
+    }
+
+  // go through the list until there is only one node in the list
+  // find the next operator
+  // If no operator can be found, return false
+  // If an operator is found, find the two previous nodes as operands
+  // If cannot find previous two operands, return false
+  // If two operands can be found, perform the arithmetic operation
+  // Be careful, subtraction is no commutative: 4 2 - means 4 - 2,
+  //    not 2 - 4
+  // After the operation,
+  //     put the result back to the list
+  //     remove the two nodes used to store the two operands
+  // After going through the entire list and performing the operations,
+  //     the list should have exactly one node left. If this is not
+  //     true, return false
+  // If the input is valud, return true
+
+
+
+  // if more than one node left, return false
+
+  // if the remaining node is an operator, return false
+
+  // if everything is OK, return true
+  return true;
+}
+#endif
---
+++ Solutions/Part3/HW14Arithmetic2/list.c
@@ -0,0 +1,178 @@
+// ***
+// *** You MUST modify this file
+// ***
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+#ifdef TEST_READLIST
+
+ListNode * Construct (ListNode * ptr)
+{
+  ListNode * p = malloc(sizeof(ListNode));
+  if (p == NULL) {
+    return ptr;
+  }
+  p -> prev = NULL;
+  p -> next = NULL;
+  ptr = p;
+  return ptr;
+}
+
+// read line by line from the input file
+// each line shorter than WORDLENGTH (including '\n' and '\0')
+// arithlist should have memory to store head and tail
+// If arithlist is NULL, return false
+// If fopen fails, return false
+// If a line is too long,
+//    free memory of the list
+//    fclose
+//    return false
+// If everything is fine
+//    fclose
+//    arithlist points to the head and tail of the list
+//    return true
+
+bool readList(char * filename, List * arithlist)
+{
+  FILE * fptr = fopen(filename, "r");
+  //fopen fails.
+  if (fptr == NULL) {
+    return false;
+  }
+  if (arithlist == NULL) {
+    return false;
+  }
+  //read list from the file.
+  char word [WORDLENGTH];
+  while (! feof(fptr)) {
+    if (fgets(word, WORDLENGTH, fptr) != NULL) {
+      addNode(arithlist, word);
+    }
+  }
+  fclose(fptr);
+
+
+  return true;
+}
+#endif
+
+#ifdef TEST_DELETELIST
+// If arithlist is NULL, do nothing
+// release the memory of every node in the list
+// release the memory of the list
+void deleteList(List * arithlist)
+{
+  if (arithlist == NULL) {
+    return;
+  }
+  ListNode * p = arithlist -> head;
+  ListNode * q = p -> next;
+  while (q != NULL) {
+    free(p);
+    // the two lines following cannot change order.
+    p = q;
+    q = p -> next;
+  }
+  //free the last one. now p->next should be NULL.
+  free(p);
+  free(arithlist);
+}
+#endif
+
+#ifdef TEST_ADDNODE
+// Input:
+// arithlist stores the addresses of head and tail
+// If arithlist is NULL, do nothing
+// word is the word to be added
+//
+// Output:
+// a ListNode is added to the end (become tail)
+//
+// allocate memory for a new ListNode
+// copy word to the word attribute of the new ListNode
+// insert the ListNode to the list
+void addNode(List * arithlist, char * word)
+{
+  if (arithlist == NULL) {
+    return;
+  }
+  //when airthlist is empty, add one node whose prev and next are NULL.
+  //head and tail point to the same node added.
+  //words is empty.
+  if (arithlist -> head == NULL) {
+    arithlist -> head = Construct(arithlist -> head);
+    arithlist -> tail = arithlist -> head;
+    ListNode * p = arithlist -> head;
+    strcpy(p -> word, word);
+  }
+  // if arithlist is not empty, add one node to the tail.
+  // make tail pointed to the new node.
+  else {
+    ListNode * p = arithlist -> tail;
+    ListNode * q = Construct((arithlist -> tail) -> next);
+    p -> next = q;
+    q -> prev = p;
+    q -> next = NULL;
+    arithlist -> tail = q;
+    strcpy(q -> word, word);
+  }
+  return;
+}
+#endif
+
+#ifdef TEST_DELETENODE
+//  Input:
+// arithlist stores the addresses of head and tail
+// If arithlist is NULL, return false
+// If the list is empty (head and tail are NULL), return false
+// ln is the node to be deleted
+// If ln is not in the list, return false
+//
+// Output:
+// arithlist stores the addresses of head and tail
+//   after ln is deleted
+// return true.
+//
+// Be careful about delete the first or the last node
+bool deleteNode(List * arithlist, ListNode * ln)
+{
+  if (arithlist == NULL) {
+    return false;
+  }
+  if ((arithlist -> head == NULL) || (arithlist -> tail == NULL)) {
+    return false;
+  }
+  ListNode * p = arithlist -> head;
+  ListNode * q = p -> next;
+  ListNode * r = arithlist -> tail;
+  if (p == ln) {
+    q -> prev = NULL;
+    arithlist -> head = q;
+    free(p);
+  }
+  else if (r == ln) {
+    (r -> prev) -> next = NULL;
+    arithlist -> tail = r -> prev;
+    free(r);
+  }
+  // find the node to be deleted.
+  else {
+    while (q != ln) {
+      p = q;
+      q = p -> next;
+    }
+    if (q == NULL) {
+      return false;   // listnode is not in arithlist.
+    }
+    // at this case q must be eaqual to ListNode * ln.
+    // p is equal to q -> prev.
+    p -> next = q -> next;
+    (q -> next) -> prev = p;
+    free(q);
+  }
+  return true;
+}
+#endif

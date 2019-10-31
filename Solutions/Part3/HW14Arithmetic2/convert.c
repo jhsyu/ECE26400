c497e8a4472cd15da391557ec207953e11968159
xu1392 <xu1392@purdue.edu>
2019-10-30 21:13:45 -0400

    frame completed

--- Solutions/Part3/HW14Arithmetic2/convert.c
+++ Solutions/Part3/HW14Arithmetic2/convert.c
@@ -14,7 +14,9 @@ const int Operations[] = {'+', '-', '*', '(', ')'};
 // return -1 if the word is not an operator
 // return 0 if the word contains '+'
 // return 1 if the word contains '-'
-// ...
+// retrun 2 if the word contains '*'
+// return 3 if the word contains '('
+// return 4 if the word contains ')'
 int isOperator(char * word)
 {
   int ind;
@@ -33,6 +35,7 @@ int isOperator(char * word)
 // ***
 // *** You MUST modify the convert function
 // ***
+
 #ifdef TEST_CONVERT
 bool convert(List * arithlist)
 {
@@ -44,6 +47,63 @@ bool convert(List * arithlist)
     {
       return true;
     }
+    // the output stack is arithlist.
+    // arithlist also stores the arithlist need to convert.
+    // create another list to store operators.
+    List * operators = malloc(sizeof(List));
+    operators -> head = NULL;
+    operators -> tail = NULL;
+    //number list
+    List * output = malloc (sizeof(List));
+    numbers -> head = NULL;
+    numbers -> tail = NULL;
+    //pointers point to arithlist
+    ListNode * p = arithlist -> head;
+    ListNode * q = p -> next;
+    // pointers point to operator list.
+    ListNode * m = operators -> head;
+    ListNode * n = NULL;
+    // pointers point to number list.
+    ListNode * a = output -> head;
+    ListNode * b = NULL;
+
+    // start convert.
+    while (q != NULL) {
+      // read one word from arithlist.
+      char * temp;
+      strcpy(temp, p -> word);
+      // read fails:
+      if (temp == NULL) {
+        return false;
+      }
+      // determine if it is a number or an operator.
+      int rtv = isOperator(temp);
+      // according to rtv decide what to do:
+      // 1. if it is a number (rtv = -1)
+      switch (rtv) {
+        case -1: //it is a number, push it to output and then read the next.
+        addNode(output, temp);
+        case 0: // it is a '+', push it to operator list.
+        addNode(operators, temp);
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        default:
+
+      }
+      p = q;
+      q = p -> next;
+    }
+  ListNode * ori = arithlist -> head;
+  arithlist -> head = output -> head;
+  arithlist -> head = output -> tail;
+  deleteList(ori);
+  deleteList(operators);
   return true;
 }
 #endif

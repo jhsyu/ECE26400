e82615cd938bf1f58c2767d7c99a9e5d7ef66b70
xu1392 <xu1392@purdue.edu>
2019-11-18 12:01:11 -0500

    segementation fault

--- Solutions/Part3/HW16BinaryTree2/tree.c
+++ Solutions/Part3/HW16BinaryTree2/tree.c
@@ -8,12 +8,18 @@
 #include <stdbool.h>
 #include "tree.h"
 
-typedef struct listnode
+typedef struct node
 {
   int value;
-  struct listnode * next;
-  struct listnode * prev;
+  struct node * next;
+  struct node * prev;
 } ListNode;
+
+typedef struct
+{
+  ListNode * head;
+  ListNode * tail;
+} List;
 
 // DO NOT MODIFY FROM HERE --->>>
 Tree * newTree(void)
@@ -73,7 +79,8 @@ int search (int v, int * inArray, int strt, int end)
   return -1;
 }
 
-TreeNode * buildHelp(int * inArray, int * postArray, int instrt, int inend, int * post)
+TreeNode * buildHelp(int * inArray, int * postArray,
+                      int instrt, int inend, int * post)
 {
   // terminating condition.
   if (instrt > inend) {
@@ -110,26 +117,69 @@ Tree * buildTree(int * inArray, int * postArray, int size)
 #endif
 
 #ifdef TEST_PRINTPATH
-TreeNode * treesearch(TreeNode tr, int v, int * count);
+ListNode * nodeconstruct(int v)
 {
-  (*count) ++;
+  ListNode * node = malloc(sizeof(ListNode));
+  node -> next = NULL;
+  node -> prev = NULL;
+  node -> value = v;
+  return node;
+}
+
+void record(int value, List * path)
+{
+  ListNode * p = nodeconstruct(value);
+  path -> tail = p;
+}
+
+void deletelist(List * list)
+{
+  if (list -> head == NULL) {
+    return;
+  }
+  if (list -> head == list -> tail) {
+    free(list -> head);
+    return;
+  }
+  ListNode * p = list -> head;
+  ListNode * q = p -> next;
+  while (q != NULL) {
+    free(p);
+    p = q;
+    q = p -> next;
+  }
+}
+TreeNode * treesearch(TreeNode * tr, int v, List * path)
+{
+  if (tr == NULL) {
+    return NULL;
+  }
+  record(v, path);
   if (tr -> value == v) {
     // node found!
     return tr;
   }
   if (tr -> value > v) {
-    return treesearch(tr -> left, v, count);
+    return treesearch(tr -> left, v, path);
   }
-  return treesearch(tr -> right, v, count);
+  return treesearch(tr -> right, v, path);
 }
+
+
 void printPath(Tree * tr, int val)
 {
-  int count = 0;
-  treesearch(tr -> root, val, &count);
-  #ifdef DEBUG
-  printf("%d\n", count);
-  #endif
-  int * pathArr = malloc(sizeof(int) * count);
-
+  List path = {
+    .head = NULL,
+    .tail = NULL,
+  };
+  treesearch(tr -> root, val, &path);
+  ListNode * p = path.head;
+  while (p != NULL) {
+    printf("%d\n", p -> value);
+    if (p -> next != NULL) {
+      p = p -> next;
+    }
+  }
+  deletelist(&path);
 }
 #endif
